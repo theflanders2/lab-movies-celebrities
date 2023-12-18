1a3248842f9cefbf30cd6fdcd7187747f08d8b4c
theflanders2 <kenneth.flanders.ii@gmail.com>
2023-12-18 22:32:48 +0100

    completed up iteration 8

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,48 +1,49 @@
-const Celebrity = require("../models/Celebrity.model");
-
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
 
 //GET route to display the form which allows for the creation of a new celebrity
-router.get('/celebrites/create', (req, res, next) => {
-    res.render('celebrities/new-celebrity')
-});
+router.get('/celebrities/create', (req, res) => res.render('celebrities/new-celebrity'));
 
 //POST route to create a celebrity using data submited via form
-router.post("/celebrities/create", (req, res) => {
-    const { name, occupation, catchPhrase } = req.body;
+router.post('/celebrities/create', (req, res, next) => {
+    console.log('New celebritity added via online form:', req.body);
+    const { name, occupation, catchPhrase } = req.body; // by using destructuring here, we are creating separate variables with the information coming from the form
+    // the same as:
+    // const name = req.body.name;
+    // const occupation = req.body.occupation;
+    // const catchPhrase = req.body.catchPhrase;
 
     Celebrity.findOne({ name })
-        .then((celebFromDb) => {
-            if (!celebFromDb) {
+        .then((aCelebFromDb) => {
+            if (!aCelebFromDb) {
                 Celebrity.create({ name, occupation, catchPhrase })
-                    .then(() =>
-                    res.redirect("/celebrities")
-                    );
+                    .then(() => res.redirect("/celebrities"));
+                    console.log('New celebrity successfully added to database.')
             } 
             else {
                 res.render("celebrities/new-celebrity", {
-                message: "Celebrity is already registered",
+                message: "This celebrity is already exists in the database.",
                 });
+                console.log('The celebrity has not been added. The celebrity already exists in the database.');
                 return;
             }
         })
-        .catch((err) =>
-            console.log(`Error while creating new celebrity: ${err}`)
-        );
+        .catch((error) => next(error));
   });
 
-  //GET route to display a list of all celebrities
+//GET route to display a list of all celebrities
 router.get('/celebrities', (req, res, next) => {
     Celebrity.find()
-        .then((celebsFromDb) => {
-            res.render('celebrities/celebrities.hbs', { celebrities: celebsFromDb })
+        .then((allCelebsFromDb) => {
+            res.render('celebrities/celebrities.hbs', { allCelebsFromDb })
+            console.log(`There are currently ${allCelebsFromDb.length} celebrities in the database.`);
+            console.log(`The ${allCelebsFromDb.length} celebrities in the database are:`, allCelebsFromDb);
         })
-        .catch((err) =>
-            console.log(`Error while getting celebrities from the database: ${err}`)
-        );
+        .catch((error) => next(error));
 });
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,62 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
+
 // all your routes here
+
+//GET route to show a form to create a movie
+router.get('/movies/create', (req, res) => {
+    const allCelebsFromDb = Celebrity.find() //pass all the celebrities from database to form
+    .then(() => res.render('movies/new-movie', { allCelebsFromDb }))
+});
+
+//POST route to send the data from the form to this route to create the movie and save it to the database
+router.post('/movies/create', (req, res, next) => {
+    const { title, genre, plot, cast } = req.body // by using destructuring here, we are creating separate variables with the information coming from the form
+    // the same as:
+    // const title = req.body.title;
+    // const genre = req.body.genre;
+    // const plot = req.body.plot;
+    // const cast = req.body.cast;
+
+    Movie.findOne({ title })
+        .then((aMovieFromDb) => {
+            if (!aMovieFromDb) {
+                Movie.create({ title, genre, plot, cast })
+                    .then(() => res.redirect("/movies/movies"));
+                    console.log('New movie successfully added to database.')
+            } 
+            else {
+                res.render("movies/new-movie", {
+                message: "This movie already exists in the database.",
+                });
+                console.log('The movie has not been added. The movie already exists in the database.');
+                return;
+            }
+        })
+        .catch((error) => next(error));
+  });
+    
+//     Movie.create({title, genre, plot, cast})
+//         .then(() => {
+//             res.redirect("/movies/movies")
+//             console.log('New movie successfully added to database.')
+//         })
+//         .catch((error) => next(error));
+// });
+
+//GET route to display a list of all movies
+router.get('/movies/movies', (req, res, next) => {
+    Movie.find()
+        .then((allMoviesFromDb) => {
+            res.render('movies/movies.hbs', { allMoviesFromDb })
+            console.log(`There are currently ${allMoviesFromDb.length} movies in the database.`);
+            console.log(`The ${allMoviesFromDb.length} movies in the database are:`, allMoviesFromDb);
+        })
+        .catch((error) => next(error));
+});
+
 
 module.exports = router;
